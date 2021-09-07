9178a234068154b2a4e1f137cd45a4330a512344
yifeicUA <yifeic@email.arizona.edu>
2021-09-07 19:49:48 +0800

    question3 update

--- practicum/week-2/question3.js
+++ practicum/week-2/question3.js
@@ -9,5 +9,14 @@ There are 5 ordered pairs: (4, 6), (4, 9), (2, 6), (2, 9), (6, 9)
 */
 
 var numberOfOrderedPairs = function(input) {
-
+    let intlist = input;
+    let ordered_pairs=0;
+    for(var i=0; i<intlist.length; i++){
+        for(var j=0; j<intlist.length; j++){
+            if((intlist[i]<intlist[j]) && (i<j)){
+                ordered_pairs+=1;
+            }
+        }
+    }
+    return ordered_pairs;
 };
