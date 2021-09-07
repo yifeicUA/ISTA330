50fd11b8dbdec56a164c0739bd1cf02b871d26eb
yifeicUA <yifeic@email.arizona.edu>
2021-09-07 18:17:11 +0800

    function test

--- practicum/week-2/question1.js
+++ practicum/week-2/question1.js
@@ -5,6 +5,13 @@ For example given the input array [5, 6, 1], the running sum is
 [5, 5+6, 5+6+1]
 */
 
-var runningSum = function(input) {
-   return [5,11,11];
+var runningSum = function(input) { 
+    let numarray = input;
+    let sumarray = new Array(numarray.length) 
+    let currsum = 0;
+    for(var i=0; i<numarray.length; i++){
+        currsum+=numarray[i];
+        sumarray[i] = currsum;
+    }
+    return sumarray;
 };
