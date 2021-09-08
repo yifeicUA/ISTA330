d0dc8c71c684bd1289407a0a5fb89a754416419b
yifeicUA <yifeic@email.arizona.edu>
2021-09-08 13:51:27 +0800

    question 8 update

--- practicum/week-2/question8.js
+++ practicum/week-2/question8.js
@@ -8,5 +8,14 @@ output: [1, 0, 1, 2]
 */
 
 var biggerAndEven = function(input) {
-
+    let sumarray = new Array(input.length) 
+    for (var i = 0; i < input.length; i++) {
+        let targetnum = 0;
+        for (var j = 0; j < input.length; j++) {
+            if ((input[j]>input[j])&&((input[j]%2 === 0))){
+                targetnum+=1;
+            }
+        }
+        sumarray[i]=targetnum;
+    }
 };
