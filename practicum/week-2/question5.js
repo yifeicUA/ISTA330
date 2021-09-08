c9eb8e9bc2fc363713a4c9b34140282dd9332047
yifeicUA <yifeic@email.arizona.edu>
2021-09-08 09:55:02 +0800

    question5 update

--- practicum/week-2/question5.js
+++ practicum/week-2/question5.js
@@ -12,6 +12,20 @@ output: 3
 */
 
 var minimalReduction = function(n) {
-
+    let nonnegative = n;
+    let operations = 0;
+    while(((nonnegative/4)%1)===0){
+        operations++;
+        nonnegative = nonnegative/4;
+    }
+    while(((nonnegative/3)%1)===0){
+        operations++;
+        nonnegative = nonnegative/3;
+    }
+    while(nonnegative!=0){
+        operations++;
+        nonnegative--;
+    }
+    return operations;
 };
 
