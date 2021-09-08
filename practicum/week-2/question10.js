076ad33ff2d86d0c7c68a48238ded9521fdabd5d
yifeicUA <yifeic@email.arizona.edu>
2021-09-08 14:15:58 +0800

    question10 update

--- practicum/week-2/question10.js
+++ practicum/week-2/question10.js
@@ -8,5 +8,16 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
-
+    let maxnum = n%10;
+    let minnum = n%10;
+    while(n>=10){
+        if(n%10 > maxnum){
+            maxnum = n%10;
+        }
+        if(n%10 < minnum){
+            minnum = n%10;
+        }
+        n = n/10;
+    }
+    return 5;
 };
