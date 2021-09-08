d7ca6a60aad496c16d1a234ac8606979d65637d7
yifeicUA <yifeic@email.arizona.edu>
2021-09-08 13:03:35 +0800

    question 6 update

--- practicum/week-2/question6.js
+++ practicum/week-2/question6.js
@@ -6,5 +6,13 @@ output: 3
 */
 
 var median = function(input) {
-
+    let sortedarray = input;
+    sortedarray.sort(function(a,b){
+        return a-b;
+    })
+    if(sortedarray.length % 2){
+        return sortedarray[parseInt(sortedarray.length/2)];
+    }else{
+        return (sortedarray[parseInt(sortedarray.length/2)]+sortedarray[parseInt(sortedarray.length/2)-1])/2;
+    }
 };
