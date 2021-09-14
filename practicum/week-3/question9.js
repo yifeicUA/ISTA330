4a62f9877f36351a193f8d8a7d58947041d9c7b5
yifeicUA <yifeic@email.arizona.edu>
2021-09-14 18:39:51 +0800

    question9 testcode 2

--- practicum/week-3/question9.js
+++ practicum/week-3/question9.js
@@ -16,7 +16,7 @@ var largestGroupsCount = function(n) {
     let map = new Map();
     let max = 1, maxCount = 0;
     for(let i = 1; i <= n; i++){
-        let sum = sumDigits(i), sumCount = map.get(sum)+1 || 1
+        let sum = summer(i), sumCount = map.get(sum)+1 || 1
         map.set(sum, sumCount)
         if(sumCount === max)maxCount++
         else if(sumCount > max)maxCount = 1
@@ -25,11 +25,11 @@ var largestGroupsCount = function(n) {
     return maxCount
 };
 
-function sumDigits(num){
+function summer(num){
     let sum = 0;
     while(num){
         sum += num%10;
-        num = Math.trunc(num / 10);
+        num = Math.parseInt(num / 10);
     }
     return sum
 }
