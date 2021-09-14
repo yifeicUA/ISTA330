2f88184cf80492d2e7af9a3736365e7c7005737d
yifeicUA <yifeic@email.arizona.edu>
2021-09-14 15:46:35 +0800

    question6 update

--- practicum/week-3/question6.js
+++ practicum/week-3/question6.js
@@ -12,5 +12,23 @@ output: 2
 */
 
 var m_element = function(input) {
-
+    let maxNum = 0;
+    let maxtimes = 0;
+    for (let i = 1; i <= input.length; i++) {
+        let num = 0;
+        for (let j = 0; j <= input.length; j++) {
+            if(input[i]==input[j]){
+                num++
+            }
+        }
+        if(num>maxtimes){
+            maxNum = input[i];
+            maxtimes=num;
+        }
+        num = 0;
+    }
+    if(maxNum<=1){
+        return -1;
+    }
+    return maxNum;
 };
