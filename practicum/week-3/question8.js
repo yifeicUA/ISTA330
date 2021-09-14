53eeaf20f5b0c8eb323ba8877588758daa442677
yifeicUA <yifeic@email.arizona.edu>
2021-09-14 18:07:40 +0800

    question8 fix2

--- practicum/week-3/question8.js
+++ practicum/week-3/question8.js
@@ -13,15 +13,15 @@ output: 3
 */
 
 var d_integer = function(input) {
+    let maxtimes = 0;
     for (let i = 0; i < input.length; i++) {
         let num = 0;
-        let maxtimes = 0;
         for (let j = 0; j < input.length; j++) {
             if(input[i]==input[j]){
                 num++;
             }
         }
-        if(maxtimes==input[i]){
+        if(num==input[i]){
             if(num>maxtimes){
                 maxtimes = num;
             }
