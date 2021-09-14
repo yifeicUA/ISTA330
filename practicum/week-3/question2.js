3443e90f2ede0ec4ba4694c62e893c01e61d5ef0
yifeicUA <yifeic@email.arizona.edu>
2021-09-14 13:20:55 +0800

    question2 test

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -10,6 +10,10 @@ Example: input: [-12,3,-1,5,-2,1,-7]
           */
 
  var largestSubarray = function(input) {
-     
-    
+        var curr_max = 0, max_so_far = 0;
+        for(var i = 0; i < input.length; i++){  
+                curr_max = Math.max(0, curr_max + input[i]);
+                max_so_far = Math.max(curr_max, max_so_far);
+        }
+        return max_so_far;
  };
