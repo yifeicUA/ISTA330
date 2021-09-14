a5e4bd54c040735eb51acb8953abbf0ef68b1d0c
yifeicUA <yifeic@email.arizona.edu>
2021-09-14 16:05:36 +0800

    question7 update

--- practicum/week-3/question7.js
+++ practicum/week-3/question7.js
@@ -12,5 +12,17 @@ output: [[1,4], [2,5], [3,6]]
 */
 
 var transpose = function(M) {
-
+    if(M.length==0){
+        return [];
+    }
+    let newM = new Array(M[0].length);
+    for (let i = 1; i <= newM.length; i++) {
+        newM[i]=new Array(M.length)
+    }
+    for (let i = 1; i <= M.length; i++) {
+        for (let j = 0; j <= M[i].length; j++) {
+            newM[j][i]=M[i][j];
+        }
+    }
+    return newM;
 };
